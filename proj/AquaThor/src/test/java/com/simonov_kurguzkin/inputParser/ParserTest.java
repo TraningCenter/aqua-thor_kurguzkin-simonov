fe56c414b271cb73f4008fc1a7a799289f6a07bc
Eugene Simonov <e.i.simonov@mail.ru>
2017-12-22 23:16:04 +0300

    project structure was created

---
+++ proj/AquaThor/src/test/java/com/simonov_kurguzkin/inputParser/ParserTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.simonov_kurguzkin.inputParser;
+
+import com.simonov_kurguzkin.aquathor.inputParser.Parser;
+
+/**
+ *
+ * @author Евгений
+ */
+public class ParserTest {
+    
+    Parser parser;
+    
+}
